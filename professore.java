cafe6d744628fa715d443a42fdd12445670375be
CarmineLamberti17 <62182828+CarmineLamberti17@users.noreply.github.com>
2020-04-28 19:49:57 +0200

    Create professore.java

---
+++ professore.java
@@ -0,0 +1,14 @@
+public class Professore {
+public String materia;
+public String scuola;
+
+public Professore (String m, String s){
+materia=m;
+scuola=s;
+}
+
+public void stampa (){
+system.out.println("Materia:" + materia);
+system.out.println("Scuola:" + scuola);
+}}
+
